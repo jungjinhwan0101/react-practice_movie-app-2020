297c91c3f7a5514c5f789aa197d6f546339e6803
JinHwanJung <yadot1478@naver.com>
2020-06-06 17:51:26 +0900

    styleing the movies

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import axios from 'axios';
 import Moive from './Movie';
+import "./App.css";
 
 class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: []
-  }
+    state = {
+        isLoading: true,
+        movies: []
+    }
 
-  getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    this.setState({isLoading: false, movies: movies});
-    console.log(movies);
-  }
+    getMovies = async () => {
+        const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        this.setState({isLoading: false, movies: movies});
+        console.log(movies);
+    }
 
-  componentDidMount() {
-    this.getMovies();
-  }
+    componentDidMount() {
+        this.getMovies();
+    }
 
-  render() {
-    const {isLoading, movies} = this.state;
-    return <div>{isLoading? "Loading...": movies.map((movie)=>(<Moive key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />))}</div>
-  }
+    render() {
+        const {isLoading, movies} = this.state;
+        return (
+        <section className="container">
+            {isLoading
+                ? (
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                </div>
+                ) : (
+                    <div className="movies">
+                        {movies.map((movie) => (
+                            <Moive
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                            />
+                        )
+                    )}
+                    </div>
+                )
+            }
+        </section>)
+    }
 }
 
 export default App;
