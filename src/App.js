50aee6794f0e63b7bb29ddb4c77b8406f11aed3d
JinHwanJung <yadot1478@naver.com>
2020-06-06 15:05:38 +0900

    movie data loading and prop types checking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import Moive from './Movie';
 
-function App() {
-  return <h1>Hello</h1>
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({isLoading: false, movies: movies});
+    console.log(movies);
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading, movies} = this.state;
+    return <div>{isLoading? "Loading...": movies.map((movie)=>(<Moive key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />))}</div>
+  }
 }
 
 export default App;
